e4f46ef9d77f37785f3607d449d0f7d2ab8cffbf
Misbahul Arifin <misbahularifin14@gmail.com>
2023-07-21 16:48:01 +0800

    booking input

--- app/Filament/Resources/BookingResource.php
+++ app/Filament/Resources/BookingResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\BookingResource\Pages;
 use App\Filament\Resources\BookingResource\RelationManagers;
 use App\Models\Booking;
+use App\Models\Room;
 use Filament\Forms;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Select;
@@ -13,6 +14,7 @@ use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -31,8 +33,10 @@ class BookingResource extends Resource
                     ->columnSpan(2)
                     ->required(),
                 Select::make('room_id')
+                    ->searchable()
                     ->relationship('room', 'name')
                     ->preload()
+                    ->getOptionLabelFromRecordUsing(fn (Room $record) => "{$record->name} ({$record->branch->name})")
                     ->columnSpan(2)
                     ->required(),
                 TextInput::make('guest_name')
@@ -50,7 +54,11 @@ class BookingResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('event_name'),
+                TextColumn::make('room.name'),
+                TextColumn::make('room.branch.name'),
+                TextColumn::make('check_in'),
+                TextColumn::make('check_out'),
             ])
             ->filters([
                 //
