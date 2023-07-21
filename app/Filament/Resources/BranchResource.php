8782c6d85750f7ecd1caf883a4c591e72e023acd
Misbahul Arifin <misbahularifin14@gmail.com>
2023-07-21 14:19:31 +0800

    first commit

---
+++ app/Filament/Resources/BranchResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BranchResource\Pages;
+use App\Filament\Resources\BranchResource\RelationManagers;
+use App\Models\Branch;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BranchResource extends Resource
+{
+    protected static ?string $model = Branch::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $label = 'Cabang ';
+
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('code'),
+                TextInput::make('name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('code'),
+                TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBranches::route('/'),
+            // 'create' => Pages\CreateBranch::route('/create'),
+            // 'edit' => Pages\EditBranch::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Providers;
+
+use App\Filament\Resources\BranchResource;
+use Filament\Facades\Filament;
+use Filament\Navigation\NavigationBuilder;
+use Filament\Navigation\NavigationGroup;
+use Filament\Navigation\NavigationItem;
+// use Filament\Pages\Dashboard;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Filament::serving(function () {
+            Filament::registerViteTheme('resources/css/filament.css');
+        });
+
+        Filament::navigation(function (NavigationBuilder $builder): NavigationBuilder {
+            return $builder->groups([
+                NavigationGroup::make('')
+                    ->collapsible(false)
+                    ->items([
+                        NavigationItem::make('Dashboard')
+                            ->icon('heroicon-o-home')
+                            ->activeIcon('heroicon-s-home')
+                            ->isActiveWhen(fn (): bool => request()->routeIs('filament.pages.dashboard'))
+                            ->url(route('filament.pages.dashboard')),
+                    ]),
+                NavigationGroup::make('Website')
+                    ->items([]),
+                NavigationGroup::make('Master Data')
+                    ->items([
+                        ...BranchResource::getNavigationItems()
+                    ]),
+            ]);
+        });
+    }
+}
